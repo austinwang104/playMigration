d3b0af25d22e82d6286f0f462409643613118a6b
austinwang104 <austin.wang@104.com.tw>
2019-07-27 17:41:36 +0800

    add first seed file, with up & down script

--- seeders/20190727092531-demo-user.js
+++ seeders/20190727092531-demo-user.js
@@ -2,6 +2,43 @@
 
 module.exports = {
   up: (queryInterface, Sequelize) => {
+    return queryInterface.bulkInsert('users', [{
+      firstName: 'John',
+      lastName: 'Doe',
+      email: 'demo@demo.com',
+      createdAt: new Date(),
+      updatedAt: new Date()
+    },{
+      firstName: 'JohnA',
+      lastName: 'DoeA',
+      email: 'demo@demo.com',
+      createdAt: new Date(),
+      updatedAt: new Date()
+    },{
+      firstName: 'JohnB',
+      lastName: 'DoeB',
+      email: 'demo@demo.com',
+      createdAt: new Date(),
+      updatedAt: new Date()
+    },{
+      firstName: 'JohnC',
+      lastName: 'DoeC',
+      email: 'demo@demo.com',
+      createdAt: new Date(),
+      updatedAt: new Date()
+    },{
+      firstName: 'JohnD',
+      lastName: 'DoeD',
+      email: 'demo@demo.com',
+      createdAt: new Date(),
+      updatedAt: new Date()
+    },{
+      firstName: 'JohnE',
+      lastName: 'DoeE',
+      email: 'demo@demo.com',
+      createdAt: new Date(),
+      updatedAt: new Date()
+    }], {});
     /*
       Add altering commands here.
       Return a promise to correctly handle asynchronicity.
@@ -15,6 +52,7 @@ module.exports = {
   },
 
   down: (queryInterface, Sequelize) => {
+    return queryInterface.bulkDelete('users', null, {});
     /*
       Add reverting commands here.
       Return a promise to correctly handle asynchronicity.
